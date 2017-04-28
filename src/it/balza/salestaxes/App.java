81b97cf10a15d02712b00a2e9c7756f45650a9a5
balza <balza@hotmail.com>
2017-04-28 18:28:35 +0200

    Root files

---
+++ src/it/balza/salestaxes/App.java
@@ -0,0 +1,13 @@
+package it.balza.salestaxes;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
